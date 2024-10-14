ff010b014bc67cd642d267efe544cbe4cb313f61
KauanAfonso <kauanafon3@gmail.com>
2024-10-14 20:49:56 -0300

    Melhorando rotas, realizando testes e upando o database

--- index.js
+++ index.js
@@ -7,31 +7,38 @@ const port = 3000
 app.use(express.json());
 
 //create
-app.post('/criar', (req,res) => {
-    const {tipoGasto , valorDoGasto, name, salario} = req.body
-    const queryTabelaGastos = 'INSERT INTO gastos (tipoDoGasto, valorDoGasto) VALUES (?, ?)'; 
-
-    conn.query(queryTabelaGastos, [tipoGasto, valorDoGasto], (err, gastoResult) =>{
-        if(err){
-            return res.status(500).send(err);
-        }
-    })
-
-
-    const queryTabelaInfoFinancas = 'INSERT INTO infoFinancas (name, salario, idGastos) VALUES (?,?)';
-    const gastoId = gastoResult.id // pegar o id referente ao gasto e mandar para 
-
-    conn.query(queryTabelaInfoFinancas, [name, salario , gastoId], (err, infoFinancasResult) => {
-        if(err){
-            return res.status(500).send
-        }
-    })
-
-    res.send("Dados criados com sucesso !")
-})
+    app.post('/criar', (req, res) => {
+        const { gastos, info } = req.body; // espera um objeto com duas chaves: gastos e info
+    
+        // Inserir cada gasto
+        gastos.forEach(({ tipoGasto, valorDoGasto }) => {
+            const queryTabelaGastos = 'INSERT INTO gastos (tipoGasto, valorDoGasto) VALUES (?, ?)';
+            
+            conn.query(queryTabelaGastos, [tipoGasto, valorDoGasto], (err, gastoResult) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+    
+                const gastoId = gastoResult.insertId; // pega o id do gasto inserido
+                const { nome, salario } = info; // pega as informações do usuário
+                
+                // Inserir na tabela infoFinancas usando o id do gasto
+                const queryTabelaInfoFinancas = 'INSERT INTO infoFinancas (nome, salario, idGastos) VALUES (?, ?, ?)';
+                
+                conn.query(queryTabelaInfoFinancas, [nome, salario, gastoId], (err, infoFinancasResult) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                });
+            });
+        });
+    
+        res.send("Dados criados com sucesso !");
+    });
+    
 
 //uptade
-app.put('/atualizar:id' , (re,res) =>{
+app.put('/atualizar:id' , (req,res) =>{
 
     const {tipoGasto, valorDoGasto, nome, salario, idInf, idGasto} = req.body
     const queryGastoUptade = "UPDATE gastos SET tipoGasto = ?, valorDoGasto = ? WHERE id = ?"
@@ -45,7 +52,7 @@ app.put('/atualizar:id' , (re,res) =>{
     const queryInfoUptade = "UPDATE infoFinancas SET nome = ?, salario = ? WHERE id = ?"
     conn.query(queryInfoUptade , [nome ,salario, idInf], (err, infoResult) => {
         if(err){
-            return res.status(500).send
+            return res.status(500).send(err)
         }
     })
 
@@ -54,4 +61,43 @@ app.put('/atualizar:id' , (re,res) =>{
 
 //DELETE
 
-app.delete()
+app.delete('/delete:id' , (req,res) =>{
+    const {id} = req.params //const id = usuario.id;
+
+    query = "DELETE from gastos WHERE id = ? ";
+
+    conn.query(query, [id], (err, deleteResult) =>{
+        if(err){
+            return res.sendStatus(500).send(err)
+        }
+
+        res.send("Excluido com sucesso !")
+    })
+ 
+})
+
+
+//READ
+app.get("/Visualizar/:id", (req,res) => {
+    const {id} = req.params;
+    const query = `
+        SELECT infoFinancas.id, infoFinancas.nome, infoFinancas.salario, 
+        gastos.tipoGasto, gastos.valorDoGasto
+        FROM infoFinancas
+        LEFT JOIN gastos ON infoFinancas.idGastos = gastos.id
+        WHERE infoFinancas.id = ?;
+        `
+
+    conn.query(query, [id], (err, readResult) =>{
+        if(err){
+            return res.sendStatus(500).send(err)
+            }
+    
+        res.json(readResult)
+    })
+     
+})
+
+app.listen(port, ()=>{
+    console.log(`Rodando na porta ${port}`)
+})
